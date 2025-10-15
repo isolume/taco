639071b872933a85872c3f7d0f03e13b0cb3b6bb
isolume <nerd@isolu.me>
2025-10-15 12:30:15 -0500

    change to arc for history

--- src/commands/forget.rs
+++ src/commands/forget.rs
@@ -1,12 +1,13 @@
+use crate::HISTORY;
 use serenity::all::{CommandOptionType, CreateCommandOption};
 use serenity::builder::CreateCommand;
 use serenity::model::application::{ResolvedOption, ResolvedValue};
-use crate::HISTORY;
 
 pub async fn run(options: &[ResolvedOption<'_>]) -> String {
     if let Some(ResolvedOption {
-        value: ResolvedValue::Channel(channel), ..
-                }) = options.first()
+        value: ResolvedValue::Channel(channel),
+        ..
+    }) = options.first()
     {
         HISTORY.lock().await.remove(&channel.id.get());
         format!("Forgetting channel: {}", channel.id)
@@ -17,11 +18,14 @@ pub async fn run(options: &[ResolvedOption<'_>]) -> String {
 }
 
 pub fn register() -> CreateCommand {
-    
     CreateCommand::new("forget")
         .description("forget everything, or just a little bit")
         .add_option(
-            CreateCommandOption::new(CommandOptionType::Channel, "channel", "The channel to forget")
-                .required(false)
+            CreateCommandOption::new(
+                CommandOptionType::Channel,
+                "channel",
+                "The channel to forget",
+            )
+            .required(false),
         )
 }
